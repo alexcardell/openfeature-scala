5358d084ae3222888d3aedce8108b62e97327acc
alexcardell <29524087+alexcardell@users.noreply.github.com>
2025-10-28 09:34:30 +0000

    Bump versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,13 +2,13 @@ package build
 
 object V {
   val cats       = "2.11.0"
-  val catsEffect = "3.6.0"
-  val http4s     = "0.23.26"
-  val circe      = "0.14.7"
-  val otel4s     = "0.12.0"
+  val catsEffect = "3.6.3"
+  val otel4s     = "0.13.2"
   val log4cats   = "2.7.0"
+  val http4s     = "0.23.32"
+  val circe     = "0.14.8"
 
-  val munit           = "1.0.0-RC1"
-  val munitCatsEffect = "2.0.0-M5"
-  val testcontainers  = "0.41.3"
+  val munit           = "1.0.0"
+  val munitCatsEffect = "2.1.0"
+  val testcontainers  = "0.43.6"
 }
